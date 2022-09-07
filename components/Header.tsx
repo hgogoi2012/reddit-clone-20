7171a2a593b8ff70fdd01ce8fe6de098c688db9a
Himanshu Gogoi <himanshu.gogoi21@gmail.com>
2022-09-07 17:15:00 +0530

    complted header

---
+++ components/Header.tsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import React from "react";
+import { useSession, signOut } from "next-auth/react";
+import {
+  HomeIcon,
+  BeakerIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/solid";
+import {
+  BellIcon,
+  ChartBarIcon,
+  GlobeAltIcon,
+  PlusIcon,
+  SparklesIcon,
+  PhoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
+
+import { signIn } from "next-auth/react";
+
+const Header = () => {
+  const { data: session } = useSession();
+
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0">
+        <Image
+          src="https://logos-world.net/imageup/Reddit/Reddit-Logo-PNG5.png"
+          layout="fill"
+        />
+      </div>
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="flex-1 ml-2 hidden lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      <form
+        action=""
+        className="flex flex-1 items-center space-x-2 rounded-sm-border border-gray-200 bg-gray-100 px-3 py-1"
+      >
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Reddit"
+          className="flex-1 bg-transparent outline-none"
+        />
+        <button type="submit" hidden />
+      </form>
+      <div className=" text-gray-500 items-center cursor-pointer space-x-2 hidden lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeAltIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChartBarIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <PhoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center lg:hidden">
+        <Bars3Icon className="icon" />
+      </div>
+      {/* Sigin Signout vutton */}
+      {!session ? (
+        <div
+          onClick={() => signIn()}
+          className="hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              src="https://links.papareact.com/23l"
+              objectFit="contain"
+              layout="fill"
+              alt=""
+            />
+          </div>
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      ) : (
+        <div
+          onClick={() => signOut()}
+          className="hidden items-center lg:flex space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              src="https://links.papareact.com/23l"
+              objectFit="contain"
+              layout="fill"
+              alt=""
+            />
+          </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate ">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400"/>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
